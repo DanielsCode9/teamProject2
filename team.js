8b034ab320ddaa2eced05e7a50d088b96d423681
CarolineHickey <61302997+CarolineHickey@users.noreply.github.com>
2020-04-02 22:19:27 -0600

    Update team.js

--- team.js
+++ team.js
@@ -5,7 +5,7 @@ class Team //The Team class should have the following instance variables:
     losses;//losses (int)
     winLossPct;//winLossPct (Double)
 
-    games = new Game(); //games (array of Game objects)
+    games = []; //new Game();
 
     //AND the following methods:
 
@@ -95,7 +95,33 @@ class BasketballTeam extends Team //The BasketballTeam class inherits from the T
 }
 
 function playGame()
-{   /*
+<<<<<<< HEAD
+{   
+
+    //have an object array for teams
+    //have an oject of team
+    //in a for loop grap the contents of the li and store the data on the new objects using the attributes. 
+    
+    
+    
+    
+    
+    
+    
+    /*
+=======
+{
+    var oTeam = new Team();
+    var aoTeam = [];
+
+
+
+    for(iCount = 0; iCount < 10; iCount++)
+    {
+        aoTeam[iCount] =
+    }
+ /*
+>>>>>>> 9fcddb643b5a495953f5e49dcd77aecb446adb92
 When the user clicks on the Play Game button you need to simulate an entire season.
 
 You will first create an array of team objects for each team displayed on the html form in the WCC group.
@@ -113,7 +139,72 @@ Each team will play the other teams in the list 2 times but they will not play e
 Once the season is over you will sort the arrays based upon the wins and display all of the teams dynamically to the output "p" tag.
 
 Make sure you use arrays of objects. Make sure you add appropriate comments. The assignment requirements are worth 90% so go the extra mile and make it look good for the extra 10 points.
-
-Only 1 per team needs to be submitted. Do NOT submit more than that. Only people that worked on the project get their name in the submission box for the group. */
+ */
 }
 
+function playGame() {
+    var iNumGames = parseInt(document.getElementById("games").value);
+    var sMyTeam = document.getElementById("teamName").value;
+
+    //Used throughout function
+    var aiMyScore = [];
+    var aiOppScore = [];
+
+    //Used in overtime loop
+    var aiNumOT = [];
+
+    //used when calculating score and for keeping track of what to say in output for each game
+    var iLossCount = 0; //used to keep track of num losses within loop
+    var iWinCount = 0; //used to keep track of num wis within loop
+    var asOutput = [];
+
+    //Usec in last part of each iteration of top for loop
+    var sOutput = "";
+    var iWinPercentage;
+
+    //for loop to create scores for both myTeam and Opp array, compare, and calc overtime in sub loop
+    for (let i = 0; i < iNumGames; i++) {
+        if (i % 2 == 0) { //use mod to determine odd (i % 2 == 1; away) or even (i % 2 == 0; home)
+            aiMyScore[i] = Math.floor((Math.random() * 101)) + 5; //I put * 101 because the score needs to be between 0 and 100
+            aiOppScore[i] = Math.floor((Math.random() * 101));
+        } else {
+            aiMyScore[i] = Math.floor((Math.random() * 101));
+            aiOppScore[i] = Math.floor((Math.random() * 101)) + 5;
+        }
+
+        var iOTCount = 0; //I declared inside loop to keep at zero each run through
+        var sOTOutput = "";
+
+        //while two scores are even execute loop until they aren't
+        while (aiMyScore[i] == aiOppScore[i]) {
+            aiMyScore[i] = aiMyScore[i] + Math.floor((Math.random() * 16)); //put 16 so the score would be from 0 to 15
+            aiOppScore[i] = aiOppScore[i] + Math.floor((Math.random() * 16));
+            aiNumOT[i] = iOTCount + 1;
+            sOTOutput = " with " + aiNumOT[i] + " OT ";
+            iOTCount++;
+        }
+
+        //now time to calc the winner of two teams
+        if (aiMyScore[i] < aiOppScore[i]) {
+            iLossCount++;
+            asOutput[i] = " and Loses";
+        } else {
+            iWinCount++;
+            asOutput[i] = " and Wins";
+        }
+
+        //plug all this into output
+        if (i == 0) {
+            sOutput = sMyTeam + ": " + aiMyScore[i] + "-" + aiOppScore[i] + sOTOutput + asOutput[i];
+        } else {
+            sOutput = sOutput + "<br>" + sMyTeam + ": " + aiMyScore[i] + "-" + aiOppScore[i] + sOTOutput + asOutput[i];
+        }
+    }
+
+    iWinPercentage = Number((iWinCount / iNumGames) * 100).toFixed(1);
+
+    sOutput = sOutput + "<br><br>" + sMyTeam + " record is " + iWinCount + "-" + iLossCount + " with a winning pct of " + iWinPercentage + "%";
+
+    //Assign the contents of the variable output to the html tag outputGames
+    document.getElementById("outputGames").innerHTML = sOutput;
+}
